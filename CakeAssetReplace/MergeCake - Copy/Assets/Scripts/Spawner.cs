85f6e3767176916227593c7b10cfe69ce0ff00da
Faiqresulzade <faigbr@code.edu.az>
2023-11-06 23:45:34 +0400

    added last task

---
+++ CakeAssetReplace/MergeCake - Copy/Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Spawner : SingletoneBase<Spawner>
+{
+    public List<GameObject> Cubes;
+    public GameObject SpawnNewObject(string name,Transform pos)
+    {
+        Debug.Log(Cubes[6].name);
+
+        GameObject gameObjectByName = Cubes.Find(obj => obj.name == name);
+
+        GameObject nextGO = Instantiate(gameObjectByName, new Vector3(pos.position.x,pos.position.y,pos.position.z),Quaternion.identity);
+        return nextGO;
+    }
+}
---
+++ CakeAssetReplace/MergeCake - Copy/Assets/Scripts/TagManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TagManager : SingletoneBase<TagManager>
+{
+    public string TagReturn(string name)
+    {
+
+        Tags tag = (Tags)Enum.Parse(typeof(Tags), name);
+        int returnNumber = (int)tag * 2;
+        string ObjName = Enum.GetName(typeof(Tags), returnNumber);
+        return ObjName;
+    }
+    public enum Tags : int
+    {
+        OneSlice = 2,
+        TwoSlice = 4,
+        FourSlice = 8,
+        AllSlice=16,
+        CompleteCake = 32,
+        Last = 64,
+        CherryHalf=128,
+        CherryFull=256,
+        Upgraded_strawberry=512,
+    }
+}
